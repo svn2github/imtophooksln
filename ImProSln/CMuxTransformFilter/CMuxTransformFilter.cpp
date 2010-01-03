e01d3f74c804a19242057e2702c7f93517ac6955
ndhumuscle <ndhumuscle@fa729b96-8d43-11de-b54f-137c5e29c83a>
2010-01-03 15:15:03 +0000

    [what] add Multi Input and Output BaseFilter not finish yet ( CMuxTransformOutputPin still empty)
    git-svn-id: http://imtophooksln.googlecode.com/svn/trunk@39 fa729b96-8d43-11de-b54f-137c5e29c83a

---
+++ ImProSln/CMuxTransformFilter/CMuxTransformFilter.cpp
@@ -0,0 +1,617 @@
+#include "CMuxTransformFilter.h"
+
+
+CMuxTransformFilter::CMuxTransformFilter(__in_opt LPCTSTR pName, __inout_opt LPUNKNOWN pUnk, REFCLSID  clsid) :
+CBaseFilter(pName, pUnk, &m_csFilter, clsid), m_bEOSDelivered(FALSE), m_bQualityChanged(FALSE), 
+m_bSampleSkipped(FALSE)
+{
+	
+}
+#ifdef UNICODE
+CMuxTransformFilter::CMuxTransformFilter(__in_opt LPCSTR pName,
+								   __inout_opt LPUNKNOWN pUnk,
+								   REFCLSID  clsid) :
+CBaseFilter(pName,pUnk,&m_csFilter, clsid), m_bEOSDelivered(FALSE), m_bQualityChanged(FALSE),
+m_bSampleSkipped(FALSE)
+{
+#ifdef PERF
+	RegisterPerfId();
+#endif //  PERF
+}
+#endif
+
+CMuxTransformFilter::~CMuxTransformFilter()
+{
+	for (int i =0; i < m_pInputPins.size(); i++)
+	{
+		m_pInputPins.at(i)->Release();
+		m_pInputPins.at(i) = NULL;
+	}
+	m_pOutputPins.clear();
+	for (int i =0; i < m_pOutputPins.size(); i++)
+	{
+		m_pOutputPins[i]->Release();
+		m_pOutputPins[i] = NULL;
+	}
+	m_pOutputPins.clear();
+}
+
+int CMuxTransformFilter::GetPinCount()
+{
+	return m_pInputPins.size() + m_pOutputPins.size();
+}
+CBasePin* CMuxTransformFilter::GetPin(int n)
+{
+	HRESULT hr = CreatePins();
+	if (FAILED(hr))
+	{
+		return NULL;
+	}
+	if (n < m_pInputPins.size())
+	{
+		return m_pInputPins[n];
+	}
+	else if ( n < GetPinCount())
+	{
+		return m_pOutputPins[GetPinCount() - n - 1];
+	}
+	else
+	{
+		return NULL;
+	}
+}
+HRESULT CMuxTransformFilter::FindPin(LPCWSTR Id, __deref_out IPin **ppPin)
+{
+	CheckPointer(ppPin,E_POINTER);
+	ValidateReadWritePtr(ppPin,sizeof(IPin *));
+	if (wcsstr(Id, L"In") != NULL)
+	{
+		int idx = -1;
+		swscanf(Id, L"In %d", &idx);
+		if (idx >= 0 && idx < m_pInputPins.size())
+		{
+			*ppPin = m_pInputPins[idx];
+		}
+	}
+	else if (wcsstr(Id, L"Out") != NULL)
+	{
+		int idx = -1;
+		swscanf(Id, L"In %d", &idx);
+		if (idx >= 0 && idx < m_pOutputPins.size())
+		{
+			*ppPin = m_pOutputPins[idx];
+		}
+	}
+	else
+	{
+		*ppPin = NULL;
+		return VFW_E_NOT_FOUND;
+	}
+
+	HRESULT hr = NOERROR;
+	//  AddRef() returned pointer - but GetPin could fail if memory is low.
+	if (*ppPin) {
+		(*ppPin)->AddRef();
+	} else {
+		hr = E_OUTOFMEMORY;  // probably.  There's no pin anyway.
+	}
+	return hr;
+}
+
+// override state changes to allow derived transform filter
+// to control streaming start/stop
+BOOL CMuxTransformFilter::IsAnyInputPinConnect()
+{
+	for (int i =0; i< m_pInputPins.size(); i++)
+	{
+		if (m_pInputPins[i]->IsConnected())
+		{
+			return TRUE;
+		}
+	}
+	return FALSE;
+}
+BOOL CMuxTransformFilter::IsAnyOutPinConnect()
+{
+	for (int i =0; i< m_pOutputPins.size(); i++)
+	{
+		if (m_pOutputPins[i]->IsConnected())
+		{
+			return TRUE;
+		}
+	}
+	return FALSE;
+}
+HRESULT CMuxTransformFilter::Stop()
+{
+	CAutoLock lck1(&m_csFilter);
+	if (m_State == State_Stopped) {
+		return NOERROR;
+	}
+
+	// Succeed the Stop if we are not completely connected
+	ASSERT(m_pInputPins.size() == 0 || m_pOutputPins.size() != 0);
+	if ( IsAnyInputPinConnect() == FALSE || IsAnyOutPinConnect() == FALSE) {
+			m_State = State_Stopped;
+			m_bEOSDelivered = FALSE;
+			return NOERROR;
+	}
+
+	ASSERT(m_pInputPins.size());
+	ASSERT(m_pOutputPins.size());
+
+	// decommit the input pin before locking or we can deadlock
+	for (int i = 0; i < m_pInputPins.size(); i++)
+	{
+		m_pInputPins[i]->Inactive();
+	}
+	// synchronize with Receive calls
+
+	CAutoLock lck2(&m_csReceive);
+	for (int i = 0 ; i < m_pOutputPins.size(); i++)
+	{
+		m_pOutputPins[i]->Inactive();
+	}
+
+	// allow a class derived from CTransformFilter
+	// to know about starting and stopping streaming
+	HRESULT hr = StopStreaming();
+	if (SUCCEEDED(hr)) {
+		// complete the state transition
+		m_State = State_Stopped;
+		m_bEOSDelivered = FALSE;
+	}
+	return hr;
+}
+
+
+HRESULT CMuxTransformFilter::Pause()
+{
+	CAutoLock lck(&m_csFilter);
+	HRESULT hr = NOERROR;
+
+	if (m_State == State_Paused) {
+		// (This space left deliberately blank)
+	}
+
+	// If we have no input pin or it isn't yet connected then when we are
+	// asked to pause we deliver an end of stream to the downstream filter.
+	// This makes sure that it doesn't sit there forever waiting for
+	// samples which we cannot ever deliver without an input connection.
+
+	else if (IsAnyInputPinConnect() == FALSE) {
+		if (m_pOutputPins.size() && m_bEOSDelivered == FALSE) {
+			for (int i =0; i < m_pOutputPins.size(); i++)
+			{
+				m_pOutputPins[i]->DeliverEndOfStream();
+			}
+			m_bEOSDelivered = TRUE;
+		}
+		m_State = State_Paused;
+	}
+
+	// We may have an input connection but no output connection
+	// However, if we have an input pin we do have an output pin
+
+	else if (IsAnyOutPinConnect() == FALSE) {
+		m_State = State_Paused;
+	}
+
+	else {
+		if (m_State == State_Stopped) {
+			// allow a class derived from CTransformFilter
+			// to know about starting and stopping streaming
+			CAutoLock lck2(&m_csReceive);
+			hr = StartStreaming();
+		}
+		if (SUCCEEDED(hr)) {
+			hr = CBaseFilter::Pause();
+		}
+	}
+
+	m_bSampleSkipped = FALSE;
+	m_bQualityChanged = FALSE;
+	return hr;
+}
+
+
+HRESULT CMuxTransformFilter::StartStreaming()
+{
+	return NOERROR;
+}
+
+
+HRESULT CMuxTransformFilter::StopStreaming()
+{
+	return NOERROR;
+}
+
+HRESULT CMuxTransformFilter::AlterQuality(Quality q)
+{
+	UNREFERENCED_PARAMETER(q);
+	return S_FALSE;
+}
+HRESULT CMuxTransformFilter::EndOfStream(void)
+{
+	HRESULT hr = NOERROR;
+	for (int i =0 ; i < m_pOutputPins.size(); i++ )
+	{
+		hr = m_pOutputPins[i]->DeliverEndOfStream();
+		if (FAILED(hr))
+		{
+			return hr;
+		}
+	}
+	return hr;
+}
+HRESULT CMuxTransformFilter::BeginFlush(void)
+{
+	HRESULT hr = NOERROR;
+	for (int i =0; i< m_pOutputPins.size(); i++)
+	{
+		// block receives -- done by caller (CBaseInputPin::BeginFlush)
+
+		// discard queued data -- we have no queued data
+
+		// free anyone blocked on receive - not possible in this filter
+
+		// call downstream
+		hr = m_pOutputPins[i]->DeliverBeginFlush();
+	}
+	return hr;
+}
+
+HRESULT CMuxTransformFilter::EndFlush(void)
+{
+	// sync with pushing thread -- we have no worker thread
+
+	// ensure no more data to go downstream -- we have no queued data
+
+	// call EndFlush on downstream pins
+	ASSERT (m_pOutputPins.size() != 0);
+	for (int i = 0 ; i < m_pOutputPins.size(); i++)
+	{
+		return m_pOutputPins[i]->DeliverEndFlush();
+	}
+	// caller (the input pin's method) will unblock Receives
+}
+
+HRESULT CMuxTransformFilter::NewSegment(
+							 REFERENCE_TIME tStart,
+							 REFERENCE_TIME tStop,
+							 double dRate)
+{
+	for (int i = 0; i < m_pOutputPins.size(); i++)
+	{
+		m_pOutputPins[i]->DeliverNewSegment(tStart, tStop, dRate);
+	}
+	return S_OK;
+}
+
+
+HRESULT CMuxTransformFilter::SetMediaType(PIN_DIRECTION direction,IPin* pPin, const CMediaType *pmt)
+{
+	UNREFERENCED_PARAMETER(direction);
+	UNREFERENCED_PARAMETER(pmt);
+	UNREFERENCED_PARAMETER(pPin);
+	return NOERROR;
+}
+
+
+HRESULT CMuxTransformFilter::CheckConnect(PIN_DIRECTION dir, IPin* pMyPin, IPin *pOtherPin)
+{
+	UNREFERENCED_PARAMETER(dir);
+	UNREFERENCED_PARAMETER(pOtherPin);
+	UNREFERENCED_PARAMETER(pMyPin);
+	return NOERROR;
+}
+
+
+HRESULT CMuxTransformFilter::BreakConnect(PIN_DIRECTION dir, IPin* pPin)
+{
+	UNREFERENCED_PARAMETER(dir);
+	UNREFERENCED_PARAMETER(pPin);
+	return NOERROR;
+}
+
+
+// Let derived classes know about connection completion
+
+HRESULT CMuxTransformFilter::CompleteConnect(PIN_DIRECTION dir, IPin* pMyPin, IPin *pOtherPin)
+{
+	UNREFERENCED_PARAMETER(dir);
+	UNREFERENCED_PARAMETER(pMyPin);
+	UNREFERENCED_PARAMETER(pOtherPin);
+	return NOERROR;
+}
+
+
+// Set up our output sample
+HRESULT CMuxTransformFilter::InitializeOutputSample(IMediaSample *pSample, IPin* pInputPin, IPin* pOutputPin, __deref_out IMediaSample **ppOutSample )
+{
+	IMediaSample *pOutSample;
+	CMuxTransformInputPin* pInPin = (CMuxTransformInputPin*) pInputPin;
+	CMuxTransformOutputPin* pOutPin = (CMuxTransformOutputPin*) pOutPin;
+	// default - times are the same
+
+	AM_SAMPLE2_PROPERTIES * const pProps = pInPin->SampleProps();
+	DWORD dwFlags = m_bSampleSkipped ? AM_GBF_PREVFRAMESKIPPED : 0;
+
+	// This will prevent the image renderer from switching us to DirectDraw
+	// when we can't do it without skipping frames because we're not on a
+	// keyframe.  If it really has to switch us, it still will, but then we
+	// will have to wait for the next keyframe
+	if (!(pProps->dwSampleFlags & AM_SAMPLE_SPLICEPOINT)) {
+		dwFlags |= AM_GBF_NOTASYNCPOINT;
+	}
+
+	ASSERT(pOutPin->m_pAllocator != NULL);
+	HRESULT hr = pOutPin->m_pAllocator->GetBuffer(
+		&pOutSample
+		, pProps->dwSampleFlags & AM_SAMPLE_TIMEVALID ?
+		&pProps->tStart : NULL
+		, pProps->dwSampleFlags & AM_SAMPLE_STOPVALID ?
+		&pProps->tStop : NULL
+		, dwFlags
+		);
+	*ppOutSample = pOutSample;
+	if (FAILED(hr)) {
+		return hr;
+	}
+
+	ASSERT(pOutSample);
+	IMediaSample2 *pOutSample2;
+	if (SUCCEEDED(pOutSample->QueryInterface(IID_IMediaSample2,
+		(void **)&pOutSample2))) {
+			/*  Modify it */
+			AM_SAMPLE2_PROPERTIES OutProps;
+			EXECUTE_ASSERT(SUCCEEDED(pOutSample2->GetProperties(
+				FIELD_OFFSET(AM_SAMPLE2_PROPERTIES, tStart), (PBYTE)&OutProps)
+				));
+			OutProps.dwTypeSpecificFlags = pProps->dwTypeSpecificFlags;
+			OutProps.dwSampleFlags =
+				(OutProps.dwSampleFlags & AM_SAMPLE_TYPECHANGED) |
+				(pProps->dwSampleFlags & ~AM_SAMPLE_TYPECHANGED);
+			OutProps.tStart = pProps->tStart;
+			OutProps.tStop  = pProps->tStop;
+			OutProps.cbData = FIELD_OFFSET(AM_SAMPLE2_PROPERTIES, dwStreamId);
+			hr = pOutSample2->SetProperties(
+				FIELD_OFFSET(AM_SAMPLE2_PROPERTIES, dwStreamId),
+				(PBYTE)&OutProps
+				);
+			if (pProps->dwSampleFlags & AM_SAMPLE_DATADISCONTINUITY) {
+				m_bSampleSkipped = FALSE;
+			}
+			pOutSample2->Release();
+	} else {
+		if (pProps->dwSampleFlags & AM_SAMPLE_TIMEVALID) {
+			pOutSample->SetTime(&pProps->tStart,
+				&pProps->tStop);
+		}
+		if (pProps->dwSampleFlags & AM_SAMPLE_SPLICEPOINT) {
+			pOutSample->SetSyncPoint(TRUE);
+		}
+		if (pProps->dwSampleFlags & AM_SAMPLE_DATADISCONTINUITY) {
+			pOutSample->SetDiscontinuity(TRUE);
+			m_bSampleSkipped = FALSE;
+		}
+		// Copy the media times
+
+		LONGLONG MediaStart, MediaEnd;
+		if (pSample->GetMediaTime(&MediaStart,&MediaEnd) == NOERROR) {
+			pOutSample->SetMediaTime(&MediaStart,&MediaEnd);
+		}
+	}
+	return S_OK;
+}
+
+
+CMuxTransformInputPin::CMuxTransformInputPin(
+									   __in_opt LPCTSTR pObjectName,
+									   __inout CMuxTransformFilter *pTransformFilter,
+									   __inout HRESULT * phr,
+									   __in_opt LPCWSTR pName)
+									   : CBaseInputPin(pObjectName, pTransformFilter, &pTransformFilter->m_csFilter, phr, pName)
+{
+	DbgLog((LOG_TRACE,2,TEXT("CMuxTransformInputPin::CMuxTransformInputPin")));
+	AMGetWideString(pName, &m_pName);
+	m_pTransformFilter = pTransformFilter;
+}
+
+
+#ifdef UNICODE
+CMuxTransformInputPin::CMuxTransformInputPin(
+									   __in_opt LPCSTR pObjectName,
+									   __inout CMuxTransformFilter *pTransformFilter,
+									   __inout HRESULT * phr,
+									   __in_opt LPCWSTR pName)
+									   : CBaseInputPin(pObjectName, pTransformFilter, &pTransformFilter->m_csFilter, phr, pName)
+{
+	DbgLog((LOG_TRACE,2,TEXT("CTransformInputPin::CTransformInputPin")));
+	AMGetWideString(pName, &m_pName);
+	m_pTransformFilter = pTransformFilter;
+}
+#endif
+
+
+HRESULT
+CMuxTransformInputPin::CheckConnect(IPin *pPin)
+{
+	HRESULT hr = m_pTransformFilter->CheckConnect(PINDIR_INPUT, this, pPin);
+	if (FAILED(hr)) {
+		return hr;
+	}
+	return CBaseInputPin::CheckConnect(pPin);
+}
+
+
+// provides derived filter a chance to release it's extra interfaces
+
+HRESULT
+CMuxTransformInputPin::BreakConnect()
+{
+	//  Can't disconnect unless stopped
+	ASSERT(IsStopped());
+	m_pTransformFilter->BreakConnect(PINDIR_INPUT, this);
+	return CBaseInputPin::BreakConnect();
+}
+
+
+// Let derived class know when the input pin is connected
+
+HRESULT
+CMuxTransformInputPin::CompleteConnect(IPin *pReceivePin)
+{
+	HRESULT hr = m_pTransformFilter->CompleteConnect(PINDIR_INPUT, this, pReceivePin);
+	if (FAILED(hr)) {
+		return hr;
+	}
+	return CBaseInputPin::CompleteConnect(pReceivePin);
+}
+
+
+// check that we can support a given media type
+
+HRESULT
+CMuxTransformInputPin::CheckMediaType(const CMediaType* pmt)
+{
+	// Check the input type
+
+	HRESULT hr = m_pTransformFilter->CheckInputType(pmt, this);
+	return hr;
+}
+
+
+// set the media type for this connection
+
+HRESULT
+CMuxTransformInputPin::SetMediaType(const CMediaType* mtIn)
+{
+	// Set the base class media type (should always succeed)
+	HRESULT hr = CBasePin::SetMediaType(mtIn);
+	if (FAILED(hr)) {
+		return hr;
+	}
+
+	// check the transform can be done (should always succeed)
+	ASSERT(SUCCEEDED(m_pTransformFilter->CheckInputType(mtIn, this)));
+
+	return m_pTransformFilter->SetMediaType(PINDIR_INPUT, this, mtIn);
+}
+
+HRESULT CMuxTransformInputPin::CheckStreaming()
+{
+	ASSERT(m_pTransformFilter->m_pOutputPins.size() != 0);
+	if (!m_pTransformFilter->IsAnyOutPinConnect()) {
+		return VFW_E_NOT_CONNECTED;
+	} else {
+		//  Shouldn't be able to get any data if we're not connected!
+		ASSERT(IsConnected());
+
+		//  we're flushing
+		if (m_bFlushing) {
+			return S_FALSE;
+		}
+		//  Don't process stuff in Stopped state
+		if (IsStopped()) {
+			return VFW_E_WRONG_STATE;
+		}
+		if (m_bRunTimeError) {
+			return VFW_E_RUNTIME_ERROR;
+		}
+		return S_OK;
+	}
+}
+
+// =================================================================
+// Implements IMemInputPin interface
+// =================================================================
+
+
+// provide EndOfStream that passes straight downstream
+// (there is no queued data)
+STDMETHODIMP
+CMuxTransformInputPin::EndOfStream(void)
+{
+	CAutoLock lck(&m_pTransformFilter->m_csReceive);
+	HRESULT hr = CheckStreaming();
+	if (S_OK == hr) {
+		hr = m_pTransformFilter->EndOfStream();
+	}
+	return hr;
+}
+
+// enter flushing state. Call default handler to block Receives, then
+// pass to overridable method in filter
+STDMETHODIMP
+CMuxTransformInputPin::BeginFlush(void)
+{
+	CAutoLock lck(&m_pTransformFilter->m_csFilter);
+	//  Are we actually doing anything?
+	ASSERT(m_pTransformFilter->m_pOutputPins.size() != 0);
+	if (!IsConnected() ||
+		!m_pTransformFilter->IsAnyOutPinConnect()) {
+			return VFW_E_NOT_CONNECTED;
+	}
+	HRESULT hr = CBaseInputPin::BeginFlush();
+	if (FAILED(hr)) {
+		return hr;
+	}
+	return m_pTransformFilter->BeginFlush();
+}
+
+
+// leave flushing state.
+// Pass to overridable method in filter, then call base class
+// to unblock receives (finally)
+STDMETHODIMP
+CMuxTransformInputPin::EndFlush(void)
+{
+	CAutoLock lck(&m_pTransformFilter->m_csFilter);
+	//  Are we actually doing anything?
+	ASSERT(m_pTransformFilter->m_pOutputPins.size() != 0);
+	if (!IsConnected() ||
+		!m_pTransformFilter->IsAnyOutPinConnect()) {
+			return VFW_E_NOT_CONNECTED;
+	}
+
+	HRESULT hr = m_pTransformFilter->EndFlush();
+	if (FAILED(hr)) {
+		return hr;
+	}
+	return CBaseInputPin::EndFlush();
+}
+
+
+// here's the next block of data from the stream.
+// AddRef it yourself if you need to hold it beyond the end
+// of this call.
+
+HRESULT
+CMuxTransformInputPin::Receive(IMediaSample * pSample)
+{
+	HRESULT hr;
+	CAutoLock lck(&m_pTransformFilter->m_csReceive);
+	ASSERT(pSample);
+
+	// check all is well with the base class
+	hr = CBaseInputPin::Receive(pSample);
+	if (S_OK == hr) {
+		hr = m_pTransformFilter->Receive(pSample, this);
+	}
+	return hr;
+}
+
+// override to pass downstream
+STDMETHODIMP
+CMuxTransformInputPin::NewSegment(
+							   REFERENCE_TIME tStart,
+							   REFERENCE_TIME tStop,
+							   double dRate)
+{
+	//  Save the values in the pin
+	CBasePin::NewSegment(tStart, tStop, dRate);
+	return m_pTransformFilter->NewSegment(tStart, tStop, dRate);
+}
+
+
---
+++ ImProSln/CMuxTransformFilter/CMuxTransformFilter.h
@@ -0,0 +1,186 @@
+#pragma once
+
+#include "dshow.h"
+#include "Streams.h"
+#include <initguid.h>
+#include "combase.h"
+#include <vector>
+using namespace std;
+
+class CMuxTransformFilter;
+class CMuxTransformInputPin : public CBaseInputPin
+{
+	friend class CMuxTransformFilter;
+
+protected:
+	CMuxTransformFilter *m_pTransformFilter;
+	
+public:
+
+	CMuxTransformInputPin(
+		__in_opt LPCTSTR pObjectName,
+		__inout CMuxTransformFilter *pTransformFilter,
+		__inout HRESULT * phr,
+		__in_opt LPCWSTR pName);
+#ifdef UNICODE
+	CMuxTransformInputPin(
+		__in_opt LPCSTR pObjectName,
+		__inout CMuxTransformFilter *pTransformFilter,
+		__inout HRESULT * phr,
+		__in_opt LPCWSTR pName);
+#endif
+
+	STDMETHODIMP QueryId(__deref_out LPWSTR * Id)
+	{
+		return AMGetWideString(m_pName, Id);
+	}
+
+	// Grab and release extra interfaces if required
+
+	HRESULT CheckConnect(IPin *pPin);
+	HRESULT BreakConnect();
+	HRESULT CompleteConnect(IPin *pReceivePin);
+
+	// check that we can support this output type
+	HRESULT CheckMediaType(const CMediaType* mtIn);
+
+	// set the connection media type
+	HRESULT SetMediaType(const CMediaType* mt);
+
+	// --- IMemInputPin -----
+
+	// here's the next block of data from the stream.
+	// AddRef it yourself if you need to hold it beyond the end
+	// of this call.
+	STDMETHODIMP Receive(IMediaSample * pSample);
+
+	// provide EndOfStream that passes straight downstream
+	// (there is no queued data)
+	STDMETHODIMP EndOfStream(void);
+
+	// passes it to CTransformFilter::BeginFlush
+	STDMETHODIMP BeginFlush(void);
+
+	// passes it to CTransformFilter::EndFlush
+	STDMETHODIMP EndFlush(void);
+
+	STDMETHODIMP NewSegment(
+		REFERENCE_TIME tStart,
+		REFERENCE_TIME tStop,
+		double dRate);
+
+	// Check if it's OK to process samples
+	virtual HRESULT CheckStreaming();
+
+	// Media type
+public:
+	CMediaType& CurrentMediaType() { return m_mt; };
+};
+class CMuxTransformOutputPin : public CBaseOutputPin
+{
+	friend class CMuxTransformFilter;
+public:
+
+};
+class CMuxTransformFilter : CBaseFilter
+{
+public:
+	virtual int GetPinCount();
+	virtual CBasePin * GetPin(int n);
+	STDMETHODIMP FindPin(LPCWSTR Id, __deref_out IPin **ppPin);
+	STDMETHODIMP Stop();
+	STDMETHODIMP Pause();
+public:
+
+	CMuxTransformFilter(__in_opt LPCTSTR , __inout_opt LPUNKNOWN, REFCLSID clsid);
+#ifdef UNICODE
+	CMuxTransformFilter(__in_opt LPCSTR , __inout_opt LPUNKNOWN, REFCLSID clsid);
+#endif
+	~CMuxTransformFilter();
+	// These must be supplied in a derived class
+	virtual HRESULT Receive(IMediaSample *pSample, IPin* pReceivePin) = 0;
+	virtual HRESULT CheckInputType(const CMediaType* mtIn, IPin* pPin) PURE;
+	virtual HRESULT DecideBufferSize(
+		IMemAllocator * pAllocator, IPin* pOutPin,
+		__inout ALLOCATOR_PROPERTIES *pprop) PURE;
+	virtual HRESULT GetMediaType(int iPosition, IPin* pOutPin, __inout CMediaType *pMediaType) PURE;
+	// =================================================================
+	// ----- Optional Override Methods           -----------------------
+	// =================================================================
+
+	// you can also override these if you want to know about streaming
+	virtual HRESULT StartStreaming();
+	virtual HRESULT StopStreaming();
+
+	// override if you can do anything constructive with quality notifications
+	virtual HRESULT AlterQuality(Quality q);
+
+	// override this to know when the media type is actually set
+	virtual HRESULT SetMediaType(PIN_DIRECTION direction, IPin* pPin, const CMediaType *pmt);
+
+	// chance to grab extra interfaces on connection
+	virtual HRESULT CheckConnect(PIN_DIRECTION dir, IPin* pMyPin,IPin *pOtherPin);
+	virtual HRESULT BreakConnect(PIN_DIRECTION dir, IPin* pPin);
+	virtual HRESULT CompleteConnect(PIN_DIRECTION direction, IPin* pMyPin, IPin* pOtherPin);
+
+
+
+	// Standard setup for output sample
+	virtual HRESULT InitializeOutputSample(IMediaSample *pSample, IPin* pInputPin, IPin* pOutputPin, __deref_out IMediaSample **ppOutSample);
+
+	// if you override Receive, you may need to override these three too
+	virtual HRESULT EndOfStream(void);
+	virtual HRESULT BeginFlush(void);
+	virtual HRESULT EndFlush(void);
+	virtual HRESULT NewSegment(
+		REFERENCE_TIME tStart,
+		REFERENCE_TIME tStop,
+		double dRate);
+
+#ifdef PERF
+	// Override to register performance measurement with a less generic string
+	// You should do this to avoid confusion with other filters
+	virtual void RegisterPerfId()
+	{m_idTransform = MSR_REGISTER(TEXT("Transform"));}
+#endif // PERF
+
+
+	// implementation details
+
+protected:
+
+#ifdef PERF
+	int m_idTransform;                 // performance measuring id
+#endif
+	BOOL m_bEOSDelivered;              // have we sent EndOfStream
+	BOOL m_bSampleSkipped;             // Did we just skip a frame
+	BOOL m_bQualityChanged;            // Have we degraded?
+
+	// critical section protecting filter state.
+	CCritSec m_csFilter;
+	// critical section stopping state changes (ie Stop) while we're
+	// processing a sample.
+	//
+	// This critical section is held when processing
+	// events that occur on the receive thread - Receive() and EndOfStream().
+	//
+	// If you want to hold both m_csReceive and m_csFilter then grab
+	// m_csFilter FIRST - like CTransformFilter::Stop() does.
+
+	CCritSec m_csReceive;
+
+	// these hold our input and output pins
+
+	friend class CMuxTransformInputPin;
+	friend class CMuxTransformOutputPin;
+	vector<CMuxTransformInputPin*> m_pInputPins;
+	vector<CMuxTransformOutputPin*> m_pOutputPins;
+
+private:
+	virtual BOOL IsAnyInputPinConnect();
+	virtual BOOL IsAnyOutPinConnect();
+protected:
+	virtual HRESULT CreatePins() = 0;
+
+};
+
