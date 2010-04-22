f63e05906dbe8fb34afb16863646ec87f3d406b1
ndhumuscle <ndhumuscle@fa729b96-8d43-11de-b54f-137c5e29c83a>
2010-04-22 11:35:45 +0000

    [what] HomoWarp Project setting
    git-svn-id: http://imtophooksln.googlecode.com/svn/trunk@613 fa729b96-8d43-11de-b54f-137c5e29c83a

--- ImPro/ImProBaseClass/CMFCBasePropertyPage/DSMacro.h
+++ ImPro/ImProBaseClass/CMFCBasePropertyPage/DSMacro.h
@@ -5,6 +5,3 @@
 #define SLIDER_GetPos(hWndTrack)  SendMessage(hWndTrack, TBM_GETPOS, 0, 0)
 #define SLIDER_SetPos(hWndTrack, pos)  SendMessage(hWndTrack, TBM_SETPOS, (WPARAM) TRUE,(LPARAM) pos)
 #define SLIDER_SetRange(hWndTrack, iMin, iMax)  SendMessage(hWndTrack, TBM_SETRANGE, (WPARAM) TRUE, (LPARAM) MAKELONG(iMin, iMax))   
-
-
-\=========o\/'. =
